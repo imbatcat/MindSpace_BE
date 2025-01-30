7aebcf124d90a45a1e6840ecbfe914ad8e516703
Vo Thi Mai Hoa <vothimaihoa123@gmail.com>
2025-01-30 23:15:37 +0700

    #33 - Added configuration for entity Question Category

---
+++ MindSpace.Infrastructure/Configurations/QuestionCategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MindSpace.Domain.Entities;
+using MindSpace.Domain.Entities.SupportingPrograms;
+using MindSpace.Domain.Entities.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindSpace.Infrastructure.Configurations
+{
+    internal class QuestionCategoryConfiguration : IEntityTypeConfiguration<QuestionCategory>
+    {
+        public void Configure(EntityTypeBuilder<QuestionCategory> builder)
+        {
+            builder.ToTable("QuestionCategories");
+            // Properties
+            builder.Property(qc => qc.Title).IsRequired().HasMaxLength(100).IsUnicode();
+        }
+    }
+}
