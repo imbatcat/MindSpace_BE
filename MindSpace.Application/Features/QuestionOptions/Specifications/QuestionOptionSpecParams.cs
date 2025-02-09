a79e53a94ab9248e92a772695ea175f423e6ea87
Vo Thi Mai Hoa <vothimaihoa123@gmail.com>
2025-02-09 09:15:17 +0700

    #85 - refactor question specifications

--- MindSpace.Application/Features/QuestionOptions/Specifications/QuestionOptionSpecParams.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MindSpace.Application.Features.QuestionOptions.Specifications
-{
-    public class QuestionOptionSpecParams
-    {
-        public int? QuestionId { get; set; }
-        public string? SearchOptionDisplayedText { get; set; }
-    }
-}
