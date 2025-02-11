801f4165865418f8912a51e0c8cd7b4f72fb1f26
anonyfriday <duykimvu75@gmail.com>
2025-02-11 17:08:51 +0700

    Setup TestDraftService #115

---
+++ MindSpace.Application/Services/TestPeriodictDraftService.cs
@@ -0,0 +1,38 @@
+﻿using MindSpace.Domain.Entities.Drafts.TestPeriodic;
+using MindSpace.Domain.Interfaces.Services;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace MindSpace.Application.Services
+{
+    public class TestPeriodictDraftService : ITestDraftService
+    {
+        private readonly IDatabase _database;
+
+        public TestPeriodictDraftService(IConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
+
+        public async Task<bool> DeleteTestDraftAsync(string key)
+        {
+            return await _database.KeyDeleteAsync(key);
+        }
+
+        public async Task<TestDraft?> GetTestDraftAsync(string key)
+        {
+            var data = await _database.StringGetAsync(key);
+            return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<TestDraft>(data);
+        }
+
+        public async Task<TestDraft?> SetTestDraftAsync(TestDraft testDraft)
+        {
+            var created = await _database.StringSetAsync(testDraft.Id,
+                JsonSerializer.Serialize(testDraft),
+                TimeSpan.FromDays(2));
+
+            if (!created) return null;
+            return await GetTestDraftAsync(testDraft.Id);
+        }
+    }
+}
