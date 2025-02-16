7183e55dc4f66a4147c05d7685f86817d0f2706c
anonyfriday <duykimvu75@gmail.com>
2025-02-16 07:27:35 +0700

    Add GetBlogs, GetArticles, fix the enum linq transpile #70

---
+++ MindSpace.Application/Features/Resources/Queries/GetBlogs/GetBlogsQuery.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using MindSpace.Application.DTOs.Resources;
+using MindSpace.Application.DTOs;
+using MindSpace.Application.Specifications.ResourceSpecifications;
+using MindSpace.Domain.Entities.Constants;
+
+namespace MindSpace.Application.Features.Resources.Queries.GetBlogs
+{
+    public class GetBlogsQuery : IRequest<PagedResultDTO<BlogResponseDTO>>
+    {
+        // ================================
+        // === Fields & Props
+        // ================================
+
+        public ResourceSpecificationSpecParams SpecParams { get; private set; }
+
+        // ================================
+        // === Constructors
+        // ================================
+
+        public GetBlogsQuery(ResourceSpecificationSpecParams specParams)
+        {
+            SpecParams = specParams;
+            SpecParams.Type = ResourceType.Blog.ToString();
+        }
+    }
+}
--- MindSpace.Application/Specifications/ResourceSpecifications/ResourceSpecificationSpecParams.cs
+++ MindSpace.Application/Specifications/ResourceSpecifications/ResourceSpecificationSpecParams.cs
@@ -1,9 +1,11 @@
-﻿namespace MindSpace.Application.Specifications.ResourceSpecifications
+﻿using MindSpace.Domain.Entities.Constants;
+
+namespace MindSpace.Application.Specifications.ResourceSpecifications
 {
     public class ResourceSpecificationSpecParams : BasePagingParams
     {
         public string? Type { get; set; }
-        public bool? IsActive { get; set; } = true;
+        public bool? IsActive { get; set; }
         public int? SchoolManagerId { get; set; }
         public int? SpecializationId { get; set; }
         public string? SearchTitle { get; set; }
