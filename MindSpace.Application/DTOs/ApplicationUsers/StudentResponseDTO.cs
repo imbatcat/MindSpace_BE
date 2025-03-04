2a793ff9af4ccce8c1666394442bbdc3e1821e2f
Vo Thi Mai Hoa <vothimaihoa123@gmail.com>
2025-03-04 13:49:28 +0700

    #61 - fix profile mapping of account

--- MindSpace.Application/DTOs/ApplicationUsers/StudentResponseDTO.cs
+++ MindSpace.Application/DTOs/ApplicationUsers/StudentResponseDTO.cs
@@ -1,12 +1,7 @@
 ﻿namespace MindSpace.Application.DTOs.ApplicationUsers
 {
-    public class StudentResponseDTO
+    public class StudentResponseDTO : ApplicationUserResponseDTO
     {
-        public int Id { get; set; }
-        public string? FullName { get; set; }
-        public string? PhoneNumber { get; set; }
-        public string? ImageUrl { get; set; }
-        public DateOnly? DateOfBirth { get; set; }
         public SchoolDTO? School { get; set; }
     }
 }
