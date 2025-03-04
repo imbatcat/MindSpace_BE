ccd8539fa165d4d4f1ea3213cccba16248fbe897
Vo Thi Mai Hoa <vothimaihoa123@gmail.com>
2025-03-04 18:17:23 +0700

    Fixed get all accounts

--- MindSpace.Application/Features/ApplicationUsers/Queries/ViewAllAccounts/ViewAllAccountsQueryHandler.cs
+++ MindSpace.Application/Features/ApplicationUsers/Queries/ViewAllAccounts/ViewAllAccountsQueryHandler.cs
@@ -8,6 +8,7 @@ using MindSpace.Application.Interfaces.Services.Authentication;
 using MindSpace.Application.Specifications.ApplicationUserSpecifications;
 using MindSpace.Domain.Entities.Constants;
 using MindSpace.Domain.Entities.Identity;
+using StackExchange.Redis;
 
 namespace MindSpace.Application.Features.ApplicationUsers.Queries.ViewAllAccounts
 {
@@ -21,43 +22,28 @@ namespace MindSpace.Application.Features.ApplicationUsers.Queries.ViewAllAccount
     {
         public async Task<PagedResultDTO<ApplicationUserResponseDTO>> Handle(ViewAllAccountsQuery request, CancellationToken cancellationToken)
         {
-            logger.LogInformation("Getting all user accounts");
-
-            var specification = new ApplicationUserSpecification(request.SpecParams);
-
-            // Get users with the specification
-            var users = await applicationUserService.GetAllUsersWithSpecAsync(specification);
-            int count = 0;
-
-            // If role filtering is requested, filter the users by role
-            if (!string.IsNullOrEmpty(request.SpecParams.RoleId))
+            List<ApplicationUser> users;
+            if (string.IsNullOrEmpty(request.SpecParams.RoleId))
             {
-                var role = UserRoles.RoleMap[request.SpecParams.RoleId];
-                if (role != null)
-                {
-                    var usersInRole = await userManager.GetUsersInRoleAsync(role);
-                    users = users.Where(u => usersInRole.Any(ur => ur.Id == u.Id)).ToList();
-                    count = users.Count;
-                }
+                var specification = new ApplicationUserSpecification(request.SpecParams);
+                users = (List<ApplicationUser>)await applicationUserService.GetAllUsersWithSpecAsync(specification);
             }
             else
             {
-                count = await applicationUserService.CountAsync(specification);
+                var role = UserRoles.RoleMap[request.SpecParams.RoleId];
+                users = await applicationUserService.FilterUserByRoleAsync(role, request.SpecParams);
             }
-
-            logger.LogInformation("Found {Count} users", users.Count);
-
+            int count = users.Count;
             var userDtos = mapper.Map<List<ApplicationUserResponseDTO>>(users);
-
-            // Add role information to each user DTO
             foreach (var userDto in userDtos)
             {
                 var user = users.First(u => u.Id == userDto.Id);
                 var roles = await userManager.GetRolesAsync(user);
                 userDto.Role = roles.FirstOrDefault() ?? string.Empty;
             }
-
             return new PagedResultDTO<ApplicationUserResponseDTO>(count, userDtos);
         }
+
+        
     }
 }
