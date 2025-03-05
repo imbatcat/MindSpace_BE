db494701ea79b04ef38a453a6da96e4a26bc8e51
Vo Thi Mai Hoa <vothimaihoa123@gmail.com>
2025-03-05 09:34:50 +0700

    fixed student response dto mapper

--- MindSpace.Application/DTOs/ApplicationUsers/StudentResponseDTO.cs
+++ MindSpace.Application/DTOs/ApplicationUsers/StudentResponseDTO.cs
@@ -1,7 +1,12 @@
 ﻿namespace MindSpace.Application.DTOs.ApplicationUsers
 {
-    public class StudentResponseDTO : ApplicationUserResponseDTO
+    public class StudentResponseDTO 
     {
-        public SchoolDTO? School { get; set; }
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
+        public SchoolDTO? School { get; set; }    
     }
 }
