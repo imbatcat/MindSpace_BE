b3af7c7dd053d5f00db8415368388295ff7f2fb5
anonyfriday <duykimvu75@gmail.com>
2025-03-18 10:20:15 +0700

    HOTFIX - Add Specialization Controller

---
+++ MindSpace.Application/Features/Specializations/Queries/GetSpecializationsQuery.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using MindSpace.Application.DTOs.SupportingPrograms;
+using MindSpace.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MindSpace.Application.Specifications.SupportingProgramSpecifications;
+using MindSpace.Application.Specifications.SpecializationSpecifications;
+
+namespace MindSpace.Application.Features.Specializations.Queries
+{
+    public class GetSpecializationsQuery : IRequest<PagedResultDTO<SpecializationDTO>>
+    {
+        public SpecializationSpecParams SpecParams { get; private set; }
+
+        // ================================
+        // === Constructors
+        // ================================
+
+        public GetSpecializationsQuery(SpecializationSpecParams specParams)
+        {
+            this.SpecParams = specParams;
+        }
+    }
+}
