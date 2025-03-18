b6e00776fcaa66f693a5156180a8ee09ad4f47cb
imbatcat <ptdat04@gmail.com>
2025-03-18 10:38:14 +0700

    #244 Add getallschools endpoint

---
+++ MindSpace.API/Controllers/SchoolsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using MindSpace.Application.Features.Schools.Queries.ViewAllSchools;
+using MindSpace.Application.DTOs;
+using MindSpace.API.RequestHelpers;
+
+namespace MindSpace.API.Controllers
+{
+    public class SchoolsController(
+        IMediator mediator
+    ) : BaseApiController
+    {
+        [Cache(3600)]
+        [HttpGet]
+        public async Task<ActionResult<List<SchoolDTO>>> GetAllSchools()
+        {
+            var schools = await mediator.Send(new ViewAllSchoolsQuery());
+            return Ok(schools);
+        }
+
+    }
+}
---
+++ MindSpace.Application/DTOs/AddressDTO.cs
@@ -0,0 +1,11 @@
+namespace MindSpace.Application.DTOs
+{
+    public class AddressDTO
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Ward { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
