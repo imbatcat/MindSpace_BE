316f7b70b5d1c7869dd0bac87fb0a4f24eaf9f20
Vo Thi Mai Hoa <vothimaihoa123@gmail.com>
2025-03-28 22:33:48 +0700

    fixed import test api

--- MindSpace.Application/Features/Tests/Commands/CreateTestImport/CreateTestImportCommand.cs
+++ MindSpace.Application/Features/Tests/Commands/CreateTestImport/CreateTestImportCommand.cs
@@ -1,13 +1,22 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using MindSpace.Application.DTOs.Tests;
+using MindSpace.Domain.Entities.Constants;
 
 
 namespace MindSpace.Application.Features.Tests.Commands.CreateTestImport
 {
     public class CreateTestImportCommand : IRequest<TestOverviewResponseDTO>
     {
-        public CreateTestWithoutQuestionsDTO TestInfo { get; set; }
+        public string Title { get; set; }
+        public string TestCode { get; set; }
+        public TargetUser? TargetUser { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int AuthorId { get; set; }
+        public int? SchoolId { get; set; }
+        public int TestCategoryId { get; set; }
+        public int SpecializationId { get; set; }
         public IFormFile TestFile { get; set; }
     }
 }
